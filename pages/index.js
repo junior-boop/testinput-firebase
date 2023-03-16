05d3e1b36741aaf3cb8f9f7134179c90edeaef4a
junior-boop <54283084+junior-boop@users.noreply.github.com>
2023-03-16 17:11:22 +0100

    2 files modified

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
+import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import { IcRoundImage } from '../composents/icons';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  const [description, setDescription] = useState();
+  const [base64, setBase64] = useState('');
+
   const handleInputChange = ({ target }) => {
-    setNameValue(target.files[0].name);
+    setDescription(target.files[0].name);
 
     if (!target.files || !target.files[0]) return;
 
@@ -16,6 +20,11 @@ export default function Home() {
     });
   };
 
+  const HandleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e);
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -23,15 +32,28 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <div className={styles.header}>
-          <div className={styles.input}>
-            <Inputbase titre="Votre nom et Prenom" />
-            <Inputbase titre="Votre Ville" />
+        <form
+          onSubmit={HandleSubmit}
+          encType="multipart/form-data"
+          method="GET"
+        >
+          <div className={styles.header}>
+            <div className={styles.input}>
+              <Inputbase titre="Votre nom et Prenom" />
+              <Inputbase titre="Votre Ville" />
+            </div>
+            <div className={styles.images}>
+              <InputImage
+                value={description}
+                image={base64}
+                onChange={handleInputChange}
+              />
+            </div>
           </div>
-          <div className={styles.images}>
-            <InputImage onChange={handleInputChange} />
+          <div className={styles.valide}>
+            <button>Enregistrer</button>
           </div>
-        </div>
+        </form>
       </main>
 
       <footer className={styles.footer}>
@@ -56,12 +78,17 @@ function Inputbase({ titre }) {
   );
 }
 
-function InputImage({ value, onChange, image, desc = 'Description' }) {
+function InputImage({ image, onChange, value = 'Description' }) {
+  const desc = value.substring(0, 15);
+  console.log(image);
   return (
-    <div className={styles.inputImage} style={{ backgroundImage: image }}>
-      <input type="file" value={value} onChange={onChange} />
+    <div
+      className={styles.inputImage}
+      style={{ backgroundImage: `url(${image})` }}
+    >
+      <input type="file" onChange={onChange} />
       <div className={styles.bas}>
-        <div className={styles.desc}>{desc}</div>
+        <div className={styles.desc}>{desc}...</div>
         <button>
           <IcRoundImage style={{ color: 'black' }} />
         </button>
